c149c83c05321e50dd88a1c280ea2cbad245e412
fadil dlz <fadil.dlz@gmail.com>
2021-01-23 14:37:18 +0700

    Project UAS PBO

---
+++ resources/views/template.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Project PBO - Kelompok 2 ( Aplikasi Note )</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+    @yield('content')
+</div>
+
+</body>
+</html>
